882903af045af121a11c94d9fde4f1c106f47d22
André <andremessias.az@gmail.com>
2022-12-14 19:38:04 -0300

    create notification entity

--- src/application/entities/notification.ts
+++ src/application/entities/notification.ts
@@ -13,15 +13,39 @@ export class Notification {
     this.props = props;
   }
 
-  public set content(content: string) {
-    if (content.length < 5) {
-      throw new Error('Content must be at least 5 characters');
-    }
+  public set recipientId(recipientId: string) {
+    this.props.recipientId = recipientId;
+  }
 
+  public get recipientId(): string {
+    return this.props.recipientId;
+  }
+
+  public set content(content: string) {
     this.props.content = content;
   }
 
   public get content(): string {
     return this.props.content;
+  }
+
+  public set category(category: string) {
+    this.props.category = category;
+  }
+
+  public get category(): string {
+    return this.props.category;
+  }
+
+  public set readAt(readAt: Date | null | undefined) {
+    this.props.readAt = readAt;
+  }
+
+  public get readAt(): Date | null | undefined {
+    return this.props.readAt;
+  }
+
+  public get createdAt(): Date {
+    return this.props.createdAt;
   }
 }
