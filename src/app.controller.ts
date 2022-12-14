2bd161903be712e3ab42b1e99fb4d34bdcece5dc
André <andremessias.az@gmail.com>
2022-12-14 10:01:38 -0300

    change app controller

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,12 +1,12 @@
 import { Controller, Get } from '@nestjs/common';
-import { AppService } from './app.service';
+import { PrismaService } from './prisma.service';
 
-@Controller('app')
+@Controller('notifications')
 export class AppController {
-  constructor(private readonly appService: AppService) {}
+  constructor(private readonly prisma: PrismaService) {}
 
-  @Get('hello')
-  getHello(): string {
-    return this.appService.getHello();
+  @Get()
+  getHello() {
+    return this.prisma.notification.findMany();
   }
 }
