90eced10c849fee9ab64f9cdfd3f9c3251592c0d
André <andremessias.az@gmail.com>
2022-12-14 21:16:55 -0300

    finish content tests

--- src/application/entities/content.spec.ts
+++ src/application/entities/content.spec.ts
@@ -1,7 +1,17 @@
 import { Content } from './content';
 
-test('it should be able to create a notification content', () => {
-  const content = new Content('voce recebeu uma solicitação de amizade');
+describe('Notification content', () => {
+  it('should be able to create a notification content', () => {
+    const content = new Content('voce recebeu uma solicitação de amizade');
 
-  expect(content).toBeTruthy();
+    expect(content).toBeTruthy();
+  });
+
+  it('should not be able to create a notification content with less than 5 characters', () => {
+    expect(() => new Content('aoba')).toThrow();
+  });
+
+  it('should not be able to create a notification content with more than 240 characters', () => {
+    expect(() => new Content('a'.repeat(241))).toThrow();
+  });
 });
