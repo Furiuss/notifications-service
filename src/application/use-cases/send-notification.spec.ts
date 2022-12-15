fc7fa777c23c5f36ef51c0408ae8cc22611eb213
André <andremessias.az@gmail.com>
2022-12-15 14:20:32 -0300

    testing implementation

--- src/application/use-cases/send-notification.spec.ts
+++ src/application/use-cases/send-notification.spec.ts
@@ -1,9 +1,11 @@
 import { Notification } from '../entities/notification';
 import { SendNotification } from './send-notification';
 
+const notifications: Notification[] = [];
+
 const notificationsRepository = {
   async create(notification: Notification) {
-    console.log(notification);
+    notifications.push(notification);
   },
 };
 
@@ -11,12 +13,14 @@ describe('Send notifications', () => {
   it('should be able to send notification', async () => {
     const sendNotification = new SendNotification(notificationsRepository);
 
-    const { notification } = await sendNotification.execute({
+    await sendNotification.execute({
       content: 'something about social category',
       category: 'social',
       recipientId: 'faosaeasfa-nosei-assa',
     });
 
-    expect(notification).toBeTruthy();
+    console.log(notifications);
+
+    expect(notifications).toHaveLength(1);
   });
 });
