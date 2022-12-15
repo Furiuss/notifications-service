2af6fd3f98dc43ad8c4cd456f49cb830605e6ee9
André <andremessias.az@gmail.com>
2022-12-15 14:26:23 -0300

    add InMemory repository to notifications repository

--- src/application/use-cases/send-notification.spec.ts
+++ src/application/use-cases/send-notification.spec.ts
@@ -1,14 +1,6 @@
 import { Notification } from '../entities/notification';
 import { SendNotification } from './send-notification';
 
-const notifications: Notification[] = [];
-
-const notificationsRepository = {
-  async create(notification: Notification) {
-    notifications.push(notification);
-  },
-};
-
 describe('Send notifications', () => {
   it('should be able to send notification', async () => {
     const sendNotification = new SendNotification(notificationsRepository);
---
+++ test/repositories/in-memory-notifications-repository.ts
@@ -0,0 +1,12 @@
+import { Notification } from 'src/application/entities/notification';
+import { NotificationsRepository } from 'src/repositories/notifications-repository';
+
+export class InMemoryNotificationsRepository
+  implements NotificationsRepository
+{
+  public notifications: Notification[] = [];
+
+  async create(notification: Notification) {
+    this.notifications.push(notification);
+  }
+}
