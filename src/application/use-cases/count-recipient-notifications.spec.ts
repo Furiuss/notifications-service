4052ffb72e914dd9010176f1e4e9b2edca9ccb79
André <andremessias.az@gmail.com>
2022-12-16 18:51:08 -0300

    add count-recipient-notifications test

---
+++ src/application/use-cases/count-recipient-notifications.spec.ts
@@ -0,0 +1,43 @@
+import { Content } from '@application/entities/content';
+import { Notification } from '@application/entities/notification';
+import { InMemoryNotificationsRepository } from '@test/repositories/in-memory-notifications-repository';
+import { CountRecipientNotification } from './count-recipient-notifications';
+
+describe('Count recipient notifications', () => {
+  it('should be able to count recipient notifications', async () => {
+    const notificationsRepository = new InMemoryNotificationsRepository();
+    const countRecipientNotification = new CountRecipientNotification(
+      notificationsRepository,
+    );
+
+    await notificationsRepository.create(
+      new Notification({
+        category: 'social',
+        content: new Content('Marcelo dos Santos tem interesse em você'),
+        recipientId: 'recipient-1',
+      }),
+    );
+
+    await notificationsRepository.create(
+      new Notification({
+        category: 'social',
+        content: new Content('Marcelo dos Santos tem interesse em você'),
+        recipientId: 'recipient-1',
+      }),
+    );
+
+    await notificationsRepository.create(
+      new Notification({
+        category: 'social',
+        content: new Content('Marcelo dos Santos tem interesse em você'),
+        recipientId: 'recipient-2',
+      }),
+    );
+
+    const { count } = await countRecipientNotification.execute({
+      recipientId: 'recipient-1',
+    });
+
+    expect(count).toEqual(2);
+  });
+});
